8ea2febdf3a4b0fa66fdff7d1248acfd43e57cdc
zachary mines <zmines@wesleyan.edu>
2020-06-23 22:56:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,11 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1 
   end 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
 my_all?([1,2,3]) {|i| i<2}
